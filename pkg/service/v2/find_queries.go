90a8e1bc93127ff37397998be3afbf43615310af
Andrew <kpi_lithium@ukr.net>
2019-10-08 10:48:03 +0300

    fixed find declaration query

--- pkg/service/v2/find_queries.go
+++ pkg/service/v2/find_queries.go
@@ -11,6 +11,7 @@ WHERE 1=1
 {{- if .Model.CreatedAtTo }} AND created_at <= '{{call .MakeTime .Model.CreatedAtTo }}' {{- end}}	
 {{- if .Model.UserId }} AND user_id = {{ .Model.UserId }} {{- end}}	
 {{- if .Model.TypeId }} AND type_id = {{ .Model.TypeId }} {{- end}}	
+{{- if .Model.Content }} AND LOWER(content) LIKE '%' || LOWER('{{.Model.Content}}') || '%' {{- end}}	
 `
 const queryTotalCountRules = `
 SELECT COUNT(*) as count FROM rules
@@ -20,6 +21,7 @@ WHERE 1=1
 {{- if .Model.RuleId }} AND rule_id = {{ .Model.RuleId }} {{- end}}	
 {{- if .Model.CreatedBy }} AND created_by = {{ .Model.CreatedBy }} {{- end}}	
 {{- if .Model.RuleNumber }} AND rule_number = {{ .Model.RuleNumber }} {{- end}}
+{{- if .Model.Content }} AND LOWER(content) LIKE '%' || LOWER('{{.Model.Content}}') || '%' {{- end}}	
 `
 const queryTotalCountExchanges = `
 SELECT COUNT(*) as count FROM exchanges
@@ -31,6 +33,7 @@ WHERE 1=1
 {{- if .Model.RequestId }} AND request_id = {{ .Model.RequestId }} {{- end}}	
 {{- if .Model.DeclarationId }} AND declaration_id = {{ .Model.DeclarationId }} {{- end}}	
 {{- if .Model.RegisterId }} AND register_id = {{ .Model.RegisterId }} {{- end}}
+{{- if .Model.Content }} AND LOWER(content) LIKE '%' || LOWER('{{.Model.Content}}') || '%' {{- end}}	
 `
 const queryTotalCountDeclarations = `
 SELECT COUNT(*) as count FROM declarations
@@ -39,7 +42,8 @@ WHERE 1=1
 {{- if .Model.CreatedAtTo }} AND created_at <= '{{call .MakeTime .Model.CreatedAtTo }}' {{- end}}	
 {{- if .Model.DeclarationId }} AND declaration_id = {{ .Model.DeclarationId }} {{- end}}	
 {{- if .Model.UserId }} AND user_id = {{ .Model.UserId }} {{- end}}	
-{{- if .Model.UserIp }} AND user_ip = {{ .Model.UserIp }} {{- end}}	
+{{- if .Model.UserIp }} AND user_ip = '{{ .Model.UserIp }}' {{- end}}
+{{- if .Model.Content }} AND LOWER(content) LIKE '%' || LOWER('{{.Model.Content}}') || '%' {{- end}}	
 `
 
 //___________________________________________________________________________________________________
@@ -52,10 +56,12 @@ WHERE 1=1
 {{- if .Model.CreatedAtFrom }} AND created_at >= '{{ call .MakeTime .Model.CreatedAtFrom }}' {{- end}}	
 {{- if .Model.CreatedAtTo }} AND created_at <= '{{call .MakeTime .Model.CreatedAtTo }}' {{- end}}	
 {{- if .Model.UserId }} AND user_id = {{ .Model.UserId }} {{- end}}	
-{{- if .Model.TypeId }} AND type_id = {{ .Model.TypeId }} {{- end}}	
+{{- if .Model.TypeId }} AND type_id = {{ .Model.TypeId }} {{- end}}
+{{- if .Model.Content }} AND LOWER(content) LIKE '%' || LOWER('{{.Model.Content}}') || '%' {{- end}}	
+{{- if .Model.OrderBy }} ORDER BY {{ .Model.OrderBy }} {{- end}}
 {{- if .Model.Limit }} LIMIT {{ .Model.Limit }} {{- end}}	
 {{- if .Model.Offset }} OFFSET {{ .Model.Offset }} {{- end}}		
-{{- if .Model.OrderBy }} ORDER BY {{ .Model.OrderBy }} {{- end}}`
+`
 
 const queryFindRules = `
 SELECT created_at, rule_id, created_by, content, rule_number FROM rules
@@ -64,10 +70,12 @@ WHERE 1=1
 {{- if .Model.CreatedAtTo }} AND created_at <= '{{call .MakeTime .Model.CreatedAtTo }}' {{- end}}	
 {{- if .Model.RuleId }} AND rule_id = {{ .Model.RuleId }} {{- end}}	
 {{- if .Model.CreatedBy }} AND created_by = {{ .Model.CreatedBy }} {{- end}}	
-{{- if .Model.RuleNumber }} AND rule_number = {{ .Model.RuleNumber }} {{- end}}	
+{{- if .Model.RuleNumber }} AND rule_number = {{ .Model.RuleNumber }} {{- end}}
+{{- if .Model.Content }} AND LOWER(content) LIKE '%' || LOWER('{{.Model.Content}}') || '%' {{- end}}	
+{{- if .Model.OrderBy }} ORDER BY {{ .Model.OrderBy }} {{- end}}
 {{- if .Model.Limit }} LIMIT {{ .Model.Limit }} {{- end}}	
 {{- if .Model.Offset }} OFFSET {{ .Model.Offset }} {{- end}}		
-{{- if .Model.OrderBy }} ORDER BY {{ .Model.OrderBy }} {{- end}}`
+`
 
 const queryFindExchanges = `
 SELECT created_at, type_id, state_id, request_id, declaration_id, register_id, content FROM exchanges
@@ -78,10 +86,12 @@ WHERE 1=1
 {{- if .Model.StateId }} AND state_id = {{ .Model.StateId }} {{- end}}	
 {{- if .Model.RequestId }} AND request_id = {{ .Model.RequestId }} {{- end}}	
 {{- if .Model.DeclarationId }} AND declaration_id = {{ .Model.DeclarationId }} {{- end}}	
-{{- if .Model.RegisterId }} AND register_id = {{ .Model.RegisterId }} {{- end}}	
+{{- if .Model.RegisterId }} AND register_id = {{ .Model.RegisterId }} {{- end}}
+{{- if .Model.Content }} AND LOWER(content) LIKE '%' || LOWER('{{.Model.Content}}') || '%' {{- end}}	
+{{- if .Model.OrderBy }} ORDER BY {{ .Model.OrderBy }} {{- end}}
 {{- if .Model.Limit }} LIMIT {{ .Model.Limit }} {{- end}}	
 {{- if .Model.Offset }} OFFSET {{ .Model.Offset }} {{- end}}		
-{{- if .Model.OrderBy }} ORDER BY {{ .Model.OrderBy }} {{- end}}`
+`
 
 const queryFindDeclarations = `
 SELECT created_at, declaration_id, content, user_id, user_ip FROM declarations
@@ -90,7 +100,9 @@ WHERE 1=1
 {{- if .Model.CreatedAtTo }} AND created_at <= '{{call .MakeTime .Model.CreatedAtTo }}' {{- end}}	
 {{- if .Model.DeclarationId }} AND declaration_id = {{ .Model.DeclarationId }} {{- end}}	
 {{- if .Model.UserId }} AND user_id = {{ .Model.UserId }} {{- end}}	
-{{- if .Model.UserIp }} AND user_ip = {{ .Model.UserIp }} {{- end}}	
+{{- if .Model.UserIp }} AND user_ip = '{{ .Model.UserIp }}' {{- end}}
+{{- if .Model.Content }} AND LOWER(content) LIKE '%' || LOWER('{{.Model.Content}}') || '%' {{- end}}	
+{{- if .Model.OrderBy }} ORDER BY {{ .Model.OrderBy }} {{- end}}
 {{- if .Model.Limit }} LIMIT {{ .Model.Limit }} {{- end}}	
 {{- if .Model.Offset }} OFFSET {{ .Model.Offset }} {{- end}}		
-{{- if .Model.OrderBy }} ORDER BY {{ .Model.OrderBy }} {{- end}}`
+`
